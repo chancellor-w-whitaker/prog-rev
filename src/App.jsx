2c0609cc16eb950d36c65f6ca421bcebeef8bff8
Whitaker <Chancellor.Whitaker@eku.edu>
2025-02-13 10:52:43 -0500

    quick filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "ag-grid-community/styles/ag-grid.css";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-theme-quartz.css";
-import { useMemo, useRef } from "react";
+import { useMemo, useRef, useState } from "react";
 
 import { parseNumericStrings } from "./functions/parseNumericStrings";
 import { getTypes } from "./functions/getTypes";
@@ -10,6 +10,7 @@ import { onBodyScrollEnd } from "./functions/onBodyScrollEnd";
 import { autoSizeStrategy } from "./constants/autoSizeStrategy";
 import { transformData } from "./evan/transformData";
 import { usePromise } from "./usePromise";
+import { Wrapper } from "./Wrapper";
 
 const leftAlignedNumericColumns = ["Program ID", "CIP"];
 
@@ -95,28 +96,39 @@ export default function App() {
 
   const columnDefs = useMemo(() => initializeColumnDefs(types), [types]);
 
+  const [searchValue, setSearchValue] = useState("");
+
   return (
-    <div className="d-flex flex-column gap-3">
-      <div className="d-flex gap-3 align-items-center">
-        <div className="display-4 lh-1">Program Review 2024-2025</div>
-        <button
-          className="btn btn-success bg-gradient shadow-sm fs-3 d-flex align-items-center"
-          onClick={() => exportGridAsExcel({ gridRef })}
-          type="button"
-        >
-          ​
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={16}
-            height={16}
-            fill="currentColor"
-            className="bi bi-file-earmark-arrow-down-fill"
-            viewBox="0 0 16 16"
+    <Wrapper
+      heading="Program Review 2024-2025"
+      toolbar={
+        <div className="d-flex gap-2 justify-content-end">
+          <button
+            className="btn btn-success bg-gradient shadow-sm"
+            onClick={() => exportGridAsExcel({ gridRef })}
+            type="button"
           >
-            <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1m-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0" />
-          </svg>
-        </button>
-      </div>
+            ​
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={16}
+              height={16}
+              fill="currentColor"
+              className="bi bi-file-earmark-arrow-down-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1m-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0" />
+            </svg>
+          </button>
+          <input
+            className="form-control"
+            placeholder="Type to search..."
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </div>
+      }
+    >
       <div className="ag-theme-quartz" style={{ height: 500 }}>
         <AgGridReact
           autoSizeStrategy={autoSizeStrategy}
@@ -124,8 +136,9 @@ export default function App() {
           rowData={rowDataCorrected}
           columnDefs={columnDefs}
           ref={gridRef}
+          quickFilterText={searchValue}
         />
       </div>
-    </div>
+    </Wrapper>
   );
 }
